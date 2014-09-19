7b6fa84f96567b75df31651869971cf79b57afe8
Vitaly Kravtsov <in4lio@gmail.com>
2014-09-19 16:32:11 +0400

    0.3b5
    (1) makefile for the static library
    (2) makefiles cleanup

---
+++ eg/poweroff/poweroff.c
@@ -0,0 +1,48 @@
+/*
+     ____ __     ____   ___    ____ __         (((((()
+    | |_  \ \  /   ) ) | |  ) | |_  \ \  /  \(@)- /
+    |_|__  \_\/  __)_) |_|_/  |_|__  \_\/   /(@)- \
+                                               ((())))
+ *//**
+ *  \file  poweroff.c
+ *  \brief  ev3dev-c IO example.
+ *  \author  Vitaly Kravtsov (in4lio@gmail.com)
+ *  \copyright  See the LICENSE file.
+ */
+
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include "ev3.h"
+
+/*  WARNING!
+ *  Please build and install ev3dev-c static library before make this example.
+ *  See README.
+ */
+int main( void )
+{
+	char s[ 256 ];
+	char *p;
+
+	if ( ev3_init() == EV3_NONE ) return ( 1 );
+
+	if ( ev3_listdir( "/sys/class/msensor", s, sizeof( s ))) {
+		printf( "sensors: %s\n", s );
+	}
+	if ( ev3_listdir( "/sys/class/tacho-motor", s, sizeof( s ))) {
+		printf( "tacho-motors: %s\n", s );
+	}
+	printf( "Enter 'bye' to power-off the EV3 brick: " );
+	scanf( "%4s", s );
+	for ( p = s; *p; p++ ) *p = tolower( *p );
+
+	if ( strcmp( s, "bye" ) == 0 ) {
+		ev3_poweroff();
+		printf( "Goodbye!\n" );
+	} else {
+		printf( "Misspelling.\n" );
+	}
+	ev3_uninit();
+
+	return ( 0 );
+}
