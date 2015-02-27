20722f1a86f1d3f1b41221afae34d2b58d0ad42c
Vitaly Kravtsov <in4lio@gmail.com>
2015-02-27 17:01:28 +0300

    improved docs

---
+++ search/enumvalues_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tacho_5ftype_5f_5fcount_5f',['TACHO_TYPE__COUNT_',['../group__ev3__tacho.html#gga16af7b253440dadd46a80a4b9fddba4daf02a9025c2741ffb95495bdd9251abe4',1,'ev3_tacho.h']]],
+  ['trigger_5f_5fcount_5f',['TRIGGER__COUNT_',['../group__ev3__led.html#ggabc6126af1d45847bc59afa0aa3216b04a436f7ba414638b5cbbd200245ebe947d',1,'ev3_led.h']]]
+];
