df7f93b8eb78cc16564306fd1392d53164dcb013
Vitaly Kravtsov <in4lio@gmail.com>
2015-07-24 16:58:02 +0300

    updated docs

--- search/all_9.js
+++ search/all_9.js
@@ -4,6 +4,6 @@ var searchData=
   ['port_5f_5fnone_5f',['PORT__NONE_',['../group__ev3__port.html#ga15247a83ed93a7ba92aafc24655fbe11',1,'ev3_port.h']]],
   ['port_5fdesc_5f_5flimit_5f',['PORT_DESC__LIMIT_',['../group__ev3__port.html#ga8a7579e5304d01c7a8584d02551d0f73',1,'ev3_port.h']]],
   ['port_5fdir',['PORT_DIR',['../group__ev3__port.html#ga6035774ef51a86241d190f29e881368d',1,'ev3_port.h']]],
-  ['port_5fmode_5f_5fcount_5f',['PORT_MODE__COUNT_',['../group__ev3__port.html#ggabc5c98fcc1211af2b80116dd6e0a035da67d220ad3c45d800d7fed206d731b2f1',1,'ev3_port.h']]],
-  ['port_5ftype_5f_5fcount_5f',['PORT_TYPE__COUNT_',['../group__ev3__port.html#gga385c44f6fb256e5716a2302a5b940388a03ba6d319927fd5dfe5ff54239db9831',1,'ev3_port.h']]]
+  ['port_5fmode_5f_5fcount_5f',['PORT_MODE__COUNT_',['../group__ev3__port.html#ggac36f475ca5b446f4fde4c9b90bec77c8a67d220ad3c45d800d7fed206d731b2f1',1,'ev3_port.h']]],
+  ['port_5ftype_5f_5fcount_5f',['PORT_TYPE__COUNT_',['../group__ev3__port.html#ggabc5c98fcc1211af2b80116dd6e0a035da03ba6d319927fd5dfe5ff54239db9831',1,'ev3_port.h']]]
 ];
--- search/enumvalues_3.js
+++ search/enumvalues_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['port_5fmode_5f_5fcount_5f',['PORT_MODE__COUNT_',['../group__ev3__port.html#ggabc5c98fcc1211af2b80116dd6e0a035da67d220ad3c45d800d7fed206d731b2f1',1,'ev3_port.h']]],
-  ['port_5ftype_5f_5fcount_5f',['PORT_TYPE__COUNT_',['../group__ev3__port.html#gga385c44f6fb256e5716a2302a5b940388a03ba6d319927fd5dfe5ff54239db9831',1,'ev3_port.h']]]
+  ['port_5fmode_5f_5fcount_5f',['PORT_MODE__COUNT_',['../group__ev3__port.html#ggac36f475ca5b446f4fde4c9b90bec77c8a67d220ad3c45d800d7fed206d731b2f1',1,'ev3_port.h']]],
+  ['port_5ftype_5f_5fcount_5f',['PORT_TYPE__COUNT_',['../group__ev3__port.html#ggabc5c98fcc1211af2b80116dd6e0a035da03ba6d319927fd5dfe5ff54239db9831',1,'ev3_port.h']]]
 ];
