b5094b102722dec0fa5cdb897b9117dd2ae0ede2
Vitaly Kravtsov <in4lio@gmail.com>
2015-10-09 11:26:42 +0300

    updated Perl library

--- perl/eg/io.pl
+++ perl/eg/io.pl
@@ -7,31 +7,33 @@ my ( $ok, $ls ) = ev3::ev3_listdir( "/sys/class/leds", 256 );
 if ( $ok ) {
     print "leds folder: $ls\n";
 } else {
-    print "ERROR: ev3_listdir\n";
+    print "ERROR: ev3_listdir( leds )\n";
 }
-my ( $ok, $state ) = ev3::ev3_read_int( "/sys/class/leds/ev3:red:left/brightness" );
+my ( $ok, $state ) = ev3::ev3_read_int( "/sys/class/leds/ev3-left0:red:ev3dev/brightness" );
 if ( $ok ) {
-    ev3::ev3_write_int( "/sys/class/leds/ev3:red:left/brightness", ( $state ? 0 : 255 ));
+    if ( !ev3::ev3_write_int( "/sys/class/leds/ev3-left0:red:ev3dev/brightness", ( $state ? 0 : 255 ))) {
+		print "ERROR: ev3_write_int( brightness )\n";
+	}
 } else {
-    print "ERROR: ev3_read_int\n";
+    print "ERROR: ev3_read_int( brightness )\n";
 }
 my ( $ok, $ls ) = ev3::ev3_listdir( "/sys/class/lego-port", 256 );
 if ( $ok ) {
     print "lego-port folder: $ls\n";
 } else {
-    print "ERROR: ev3_listdir\n";
+    print "ERROR: ev3_listdir( lego-port )\n";
 }
 my ( $ok, $ls ) = ev3::ev3_listdir( "/sys/class/lego-sensor", 256 );
 if ( $ok ) {
     print "lego-sensor folder: $ls\n";
 } else {
-    print "ERROR: ev3_listdir\n";
+    print "ERROR: ev3_listdir( lego-sensor )\n";
 }
 my ( $ok, $ls ) = ev3::ev3_listdir( "/sys/class/tacho-motor", 256 );
 if ( $ok ) {
     print "tacho-motor folder: $ls\n";
 } else {
-    print "ERROR: ev3_listdir\n";
+    print "ERROR: ev3_listdir( tacho-motor )\n";
 }
 
 ev3::ev3_uninit();
--- perl/eg/led.pl
+++ perl/eg/led.pl
@@ -1,21 +1,22 @@
 use lib "..";
 use ev3;
 
+print "Waiting the EV3 brick online...\n";
 ev3::ev3_init() > 0 || exit( 1 );
 
-my ( $ok, $state ) = ev3::get_led_brightness( $ev3::EV3_RED_LEFT );
+my ( $ok, $state ) = ev3::get_led_brightness( $ev3::EV3_LEFT_RED );
 if ( $ok ) {
-    print "get_led_brightness( EV3_RED_LEFT ) = $state\n";
+    print "get_led_brightness( EV3_LEFT_RED ) = $state\n";
 } else {
-    print "ERROR: get_led_brightness\n";
+    print "ERROR: get_led_brightness()\n";
 }
-if ( !ev3::set_led_brightness( $ev3::EV3_RED_LEFT, 0 )) {
-    print "ERROR: set_led_brightness\n";
+if ( !ev3::set_led_brightness( $ev3::EV3_LEFT_RED, 0 )) {
+    print "ERROR: set_led_brightness()\n";
 }
-$state = ev3::get_led_trigger_inx( $ev3::EV3_RED_RIGHT );
-print "get_led_trigger_inx( EV3_RED_RIGHT ) = $state\n";
+$state = ev3::get_led_trigger_inx( $ev3::EV3_RIGHT_RED );
+print "get_led_trigger_inx( EV3_RIGHT_RED ) = $state\n";
 
-if ( !ev3::set_led_trigger( $ev3::EV3_RED_RIGHT, "heartbeat" )) {
-    print "ERROR: set_led_trigger\n";
+if ( !ev3::set_led_trigger( $ev3::EV3_RIGHT_RED, "heartbeat" )) {
+    print "ERROR: set_led_trigger()\n";
 }
 ev3::ev3_uninit();
