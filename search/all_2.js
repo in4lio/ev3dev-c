d43cc06ebc103e2a783c4e3533558045ed8f6b79
Vitaly Kravtsov <in4lio@gmail.com>
2016-05-09 20:43:09 +0300

    improved docs

--- search/all_2.js
+++ search/all_2.js
@@ -5,7 +5,8 @@ var searchData=
   ['dc_5fcommand_5f_5fcount_5f',['DC_COMMAND__COUNT_',['../group__ev3__dc.html#gga99fb83031ce9923c84392b4e92f956b5a7bc75c5a6ac5eff55c6d3eb89b4007f9',1,'ev3_dc.h']]],
   ['dc_5fdesc_5f_5flimit_5f',['DC_DESC__LIMIT_',['../group__ev3__dc.html#gafd02f6fea3d1643401117a54e595f3e0',1,'ev3_dc.h']]],
   ['dc_5fdir',['DC_DIR',['../group__ev3__dc.html#ga226579658f6c14ee44bd17ea697ab9af',1,'ev3_dc.h']]],
-  ['dc_5fstop_5fcommand_5f_5fcount_5f',['DC_STOP_COMMAND__COUNT_',['../group__ev3__dc.html#ggabc6126af1d45847bc59afa0aa3216b04adb8ebe13f8ded54b72b6812fd9779fa6',1,'ev3_dc.h']]],
+  ['dc_5fpolarity_5f_5fcount_5f',['DC_POLARITY__COUNT_',['../group__ev3__dc.html#ggabc6126af1d45847bc59afa0aa3216b04a908f097a3df07fc55d2ae9367108a446',1,'ev3_dc.h']]],
+  ['dc_5fstop_5faction_5f_5fcount_5f',['DC_STOP_ACTION__COUNT_',['../group__ev3__dc.html#ggadc29c2ff13d900c2f185ee95427fb06caed32410f9f7e2a2f697beae8304cff71',1,'ev3_dc.h']]],
   ['dc_5ftype_5f_5fcount_5f',['DC_TYPE__COUNT_',['../group__ev3__dc.html#ggadf764cbdea00d65edcd07bb9953ad2b7a51598a809a8339ee0267fb6314782995',1,'ev3_dc.h']]],
   ['dc_20motors',['DC Motors',['../group__ev3__dc.html',1,'']]]
 ];
