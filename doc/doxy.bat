6872ac8cb3106dbf936d0a86254c975214eb123d
Vitaly Kravtsov <in4lio@gmail.com>
2016-08-24 11:50:18 +0300

    updated docs

--- doc/doxy.bat
+++ doc/doxy.bat
@@ -1,6 +1,6 @@
 python -u ../yupp/yup.py --pp-no-skip-c-comment -q -d../source/ev3 mainpage.yu-dox
 rm ../html/*
 rm -r ../html/search/
-cp README ../html/
+rem cp README.md ../html/
 cp doxygen.log doxygen.log.bak
 doxygen
