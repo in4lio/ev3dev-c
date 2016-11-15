ee629478f02d2daf0fd78e25f1586daa9b8c0cb8
Vitaly Kravtsov <in4lio@gmail.com>
2016-11-15 16:09:43 +0300

    updated doc

--- doc/doxy.bat
+++ doc/doxy.bat
@@ -1,4 +1,4 @@
-python -u ../yupp/yup.py --pp-no-skip-c-comment -q -d../source/ev3 mainpage.yu-dox
+python -u ../yupp/yup.py -q -d../source/ev3 mainpage.yu-dox
 rm ../html/*
 rm -r ../html/search/
 rem cp README.md ../html/
