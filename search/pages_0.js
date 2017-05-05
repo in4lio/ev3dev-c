7917a8f1be6087f5b369e8bf542cf7a18efc651b
Vitaly Kravtsov <in4lio@gmail.com>
2017-05-05 16:45:43 +0300

    updated docs

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['how_20to_20make_20and_20install_20the_20static_20library_3f',['How to make and install the static library?',['../md_C:_Users_VIT_Dropbox_ev3_ev3dev-c_source_ev3_README.html',1,'']]]
+  ['how_20to_20make_20and_20install_20static_20library',['How to make and install STATIC library',['../md_C:_Users_VIT_Dropbox_ev3_ev3dev-c_source_ev3_README.html',1,'']]]
 ];
