81b5aafce64dbd21a2b06bd3beb4ab538c9bf5a0
Vitaly Kravtsov <in4lio@gmail.com>
2017-10-15 14:45:11 +0300

    updated Python package

--- python/ev3dev/__init__.py
+++ python/ev3dev/__init__.py
@@ -1,9 +1,9 @@
 from ev3 import *
 
 __library__ = "ev3dev-c"
-__version__ = "1.0c1"
+__version__ = "1.0c2"
 __kernel__ = "4.4.87-22-ev3dev-ev3"
-__description__ = "LEGO Mindstorms EV3 Debian C library + Python, Ruby and Perl wrappers"
+__description__ = "LEGO MINDSTORMS EV3 Debian C library + Python, Ruby and Perl wrappers"
 __author__ = "Vitaly Kravtsov"
 __author_email__ = "in4lio@gmail.com"
 __url__ = "https://github.com/in4lio/ev3dev-c"
