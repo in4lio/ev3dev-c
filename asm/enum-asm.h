155f03312950844534e8525cda802f2a268487da
Vitaly Kravtsov <in4lio@gmail.com>
2018-03-17 23:25:42 +0100

    added headers for asm

---
+++ asm/enum-asm.h
@@ -0,0 +1,19 @@
+#ifndef __ASM_ENUM_MACRO__
+#define __ASM_ENUM_MACRO__
+
+.macro ENUM_0 name
+.set ENUM_COUNTER, 0
+.equiv \name, ENUM_COUNTER
+.endm
+
+.macro ENUM_N name
+.set ENUM_COUNTER, ENUM_COUNTER + 1
+.equiv \name, ENUM_COUNTER
+.endm
+
+.macro ENUM_EQ name, value
+.set ENUM_COUNTER, \value
+.equiv \name, ENUM_COUNTER
+.endm
+
+#endif /* __ASM_ENUM_MACRO__ */
